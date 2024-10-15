7de315c173ec4d685c6a552bd68f3d333c1231ac
Ahmed Abbas <ahmedabbas8811@gmail.com>
2024-10-15 18:44:15 +0500

    Added supabase dependency, url and anonKey

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    url: 'https://vgmozfdlyvgbrbmyxtae.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZnbW96ZmRseXZnYnJibXl4dGFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg5MzQ3MzEsImV4cCI6MjA0NDUxMDczMX0.PswuUCQPndx4h6J-Xn78xe-lmV44eyUd7es8O8uhLUU',
+  );
   runApp(MyApp());
 }
 
